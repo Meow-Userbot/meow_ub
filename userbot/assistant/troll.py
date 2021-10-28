a3569dfc1d51cf8dacd42d193938cfce3433bd3d
mrcoolmj <90083311+mrcoolmj@users.noreply.github.com>
2021-10-28 21:03:55 +0530

    Update troll.py

--- userbot/assistant/troll.py
+++ userbot/assistant/troll.py
@@ -4,10 +4,10 @@ import re
 
 from telethon.events import CallbackQuery
 
-from userbot import catub
+from userbot import meowub
 
 
-@catub.tgbot.on(CallbackQuery(data=re.compile(b"troll_(.*)")))
+@meowub.tgbot.on(CallbackQuery(data=re.compile(b"troll_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
     if os.path.exists("./userbot/troll.txt"):
@@ -24,7 +24,7 @@ async def on_plug_in_callback_query_handler(event):
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in catub server"
+            reply_pop_up_alert = "This message no longer exists in meowub server"
     else:
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
