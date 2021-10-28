5553e317ec6dddf93a81c70804b163b1f88b9a4e
mrcoolmj <90083311+mrcoolmj@users.noreply.github.com>
2021-10-28 21:01:44 +0530

    Update hide.py

--- userbot/assistant/hide.py
+++ userbot/assistant/hide.py
@@ -4,10 +4,10 @@ import re
 
 from telethon.events import CallbackQuery
 
-from userbot import catub
+from userbot import meowub
 
 
-@catub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
+@meowub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
     if os.path.exists("./userbot/hide.txt"):
