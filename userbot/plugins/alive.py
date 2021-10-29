34288fc07e7a43a12dd144348f504a60a4f8d11c
mrcoolmj <90083311+mrcoolmj@users.noreply.github.com>
2021-10-29 11:15:30 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,11 +12,11 @@ from telethon.errors.rpcerrorlist import (
 )
 from telethon.events import CallbackQuery
 
-from userbot import StartTime, catub, catversion
+from userbot import StartTime, Meowub, Meowversion
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
-from ..helpers.functions import catalive, check_data_base_heal_th, get_readable_time
+from ..helpers.functions import Meowalive, check_data_base_heal_th, get_readable_time
 from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
 from . import mention
@@ -24,7 +24,7 @@ from . import mention
 plugin_category = "utils"
 
 
-@catub.cat_cmd(
+@Meowub.Meow_cmd(
     pattern="alive$",
     command=("alive", plugin_category),
     info={
@@ -40,41 +40,41 @@ async def amireallyalive(event):
     reply_to_id = await reply_id(event)
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    catevent = await edit_or_reply(event, "`Checking...`")
+    Meowevent = await edit_or_reply(event, "`Checking...`")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**✮ MY BOT IS RUNNING SUCCESSFULLY ✮**"
-    CAT_IMG = gvarstatus("ALIVE_PIC")
-    cat_caption = gvarstatus("ALIVE_TEMPLATE") or temp
-    caption = cat_caption.format(
+    Meow_IMG = gvarstatus("ALIVE_PIC")
+    Meow_caption = gvarstatus("ALIVE_TEMPLATE") or temp
+    caption = Meow_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
         EMOJI=EMOJI,
         mention=mention,
         uptime=uptime,
         telever=version.__version__,
-        catver=catversion,
+        Meowver=Meowversion,
         pyver=python_version(),
         dbhealth=check_sgnirts,
         ping=ms,
     )
-    if CAT_IMG:
-        CAT = [x for x in CAT_IMG.split()]
-        PIC = random.choice(CAT)
+    if Meow_IMG:
+        Meow = [x for x in Meow_IMG.split()]
+        PIC = random.choice(Meow)
         try:
             await event.client.send_file(
                 event.chat_id, PIC, caption=caption, reply_to=reply_to_id
             )
-            await catevent.delete()
+            await Meowevent.delete()
         except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
             return await edit_or_reply(
-                catevent,
+                Meowevent,
                 f"**Media Value Error!!**\n__Change the link by __`.setdv`\n\n**__Can't get media from this link :-**__ `{PIC}`",
             )
     else:
         await edit_or_reply(
-            catevent,
+            Meowevent,
             caption,
         )
 
@@ -82,13 +82,13 @@ async def amireallyalive(event):
 temp = """{ALIVE_TEXT}
 **{EMOJI} Database :** `{dbhealth}`
 **{EMOJI} Telethon Version :** `{telever}`
-**{EMOJI} Catuserbot Version :** `{catver}`
+**{EMOJI} Meowuserbot Version :** `{Meowver}`
 **{EMOJI} Python Version :** `{pyver}`
 **{EMOJI} Uptime :** `{uptime}`
 **{EMOJI} Master:** {mention}"""
 
 
-@catub.cat_cmd(
+@Meowub.Meow_cmd(
     pattern="ialive$",
     command=("ialive", plugin_category),
     info={
@@ -103,18 +103,18 @@ async def amireallyalive(event):
     "A kind of showing bot details by your inline bot"
     reply_to_id = await reply_id(event)
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**Catuserbot is Up and Running**"
-    cat_caption = f"{ALIVE_TEXT}\n"
-    cat_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**{EMOJI} Catuserbot Version :** `{catversion}`\n"
-    cat_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-    cat_caption += f"**{EMOJI} Master:** {mention}\n"
-    results = await event.client.inline_query(Config.TG_BOT_USERNAME, cat_caption)
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**Meowuserbot is Up and Running**"
+    Meow_caption = f"{ALIVE_TEXT}\n"
+    Meow_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
+    Meow_caption += f"**{EMOJI} Meowuserbot Version :** `{Meowversion}`\n"
+    Meow_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
+    Meow_caption += f"**{EMOJI} Master:** {mention}\n"
+    results = await event.client.inline_query(Config.TG_BOT_USERNAME, Meow_caption)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
     await event.delete()
 
 
-@catub.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
+@Meowub.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
 async def on_plug_in_callback_query_handler(event):
-    statstext = await catalive(StartTime)
+    statstext = await Meowalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
