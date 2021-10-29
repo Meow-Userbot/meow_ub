44512b31b7b00d4e0fe49dfcde2e36ff4f39bac0
mrcoolmj <90083311+mrcoolmj@users.noreply.github.com>
2021-10-29 11:04:48 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -13,14 +13,14 @@ from .. import *
 from ..Config import Config
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
-from ..core.session import catub
+from ..core.session import meowub
 from ..helpers import *
-from ..helpers.utils import _cattools, _catutils, _format, install_pip, reply_id
+from ..helpers.utils import _meowtools, _meowutils, _format, install_pip, reply_id
 
 # =================== CONSTANT ===================
-bot = catub
+bot = meowub
 LOGS = logging.getLogger(__name__)
-USERID = catub.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
+USERID = meowub.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
 ALIVE_NAME = Config.ALIVE_NAME
 AUTONAME = Config.AUTONAME
 DEFAULT_BIO = Config.DEFAULT_BIO
@@ -33,7 +33,7 @@ HEROKU_API_KEY = Config.HEROKU_API_KEY
 
 thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
 
-USERID = catub.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
+USERID = meowub.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
 
 # mention user
 mention = f"[{Config.ALIVE_NAME}](tg://user?id={USERID})"
