2b93ba7b7fbd41da4956b3f5f65056c4e9b3f457
mrcoolmj <90083311+mrcoolmj@users.noreply.github.com>
2021-10-29 08:12:32 +0530

    Update gdrive_sql.py

--- userbot/sql_helper/gdrive_sql.py
+++ userbot/sql_helper/gdrive_sql.py
@@ -1,5 +1,5 @@
 """
-credits to @mrconfused and @sandy1709
+credits to @cooolmj and @craziest_peep
 """
 #    Copyright (C) 2020  sandeep.n(π.$)
 #    This program is free software: you can redistribute it and/or modify
@@ -18,11 +18,11 @@ from . import BASE, SESSION
 
 
 class Gdrive(BASE):
-    __tablename__ = "catgdrive"
-    cat = Column(String(50), primary_key=True)
+    __tablename__ = "Meowgdrive"
+    Meow = Column(String(50), primary_key=True)
 
-    def __init__(self, cat):
-        self.cat = cat
+    def __init__(self, Meow):
+        self.Meow = Meow
 
 
 Gdrive.__table__.create(checkfirst=True)
@@ -30,7 +30,7 @@ Gdrive.__table__.create(checkfirst=True)
 
 def is_folder(folder_id):
     try:
-        return SESSION.query(Gdrive).filter(Gdrive.cat == str(folder_id))
+        return SESSION.query(Gdrive).filter(Gdrive.Meow == str(folder_id))
     except BaseException:
         return None
     finally:
@@ -55,7 +55,7 @@ def get_parent_id():
 
 
 def rmparent_id(folder_id):
-    note = SESSION.query(Gdrive).filter(Gdrive.cat == folder_id)
+    note = SESSION.query(Gdrive).filter(Gdrive.Meow == folder_id)
     if note:
         note.delete()
         SESSION.commit()
